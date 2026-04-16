107c9296e614b6ddb1af15a9f5b0601bf86e8e43
joaogablira <joaogab.lira24@gmail.com>
2026-04-16 10:18:32 -0300

    Aula 06 - 16/04

--- api_php/api.php
+++ api_php/api.php
@@ -1,16 +1,26 @@
 <?php
     //CABEÇALHO
-    header("Content-Type: application/json"); // Define o tipo de resposta
+    header("Content-Type: application/json; charset=UTF-8"); // Define o tipo de resposta
 
     $metodo = $_SERVER['REQUEST_METHOD'];
-
     // echo "Método da requisição: " . $metodo;
 
-    // //CONTEÚDO
-    $usuarios = [
-        ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
-        ["id" => 2, "nome" => "João Silva", "email" => "joao@email.com"]
-    ];
+    // RECUPERA O ARQUIVO JSON NA MESMA PASTA DO PROJETO
+    $arquivo = 'usuarios.json';
+
+    // VERIFICA SE O ARQUIVO EXISTE, SE NÃO EXISTIR CRIA UM COM ARRAY VAZIO
+    if (!file_exists($arquivo)) {
+        file_put_contents($arquivo, json_encode([], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    }
+
+    // LÊ O CONTEÚDO DO ARQUIVO JSON 
+    $usuarios = json_decode(file_get_contents($arquivo), true);
+
+    // CONTEÚDO
+    // $usuarios = [
+    //     ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
+    //     ["id" => 2, "nome" => "João Silva", "email" => "joao@email.com"]
+    // ];
 
     switch ($metodo) {
         case 'GET':
