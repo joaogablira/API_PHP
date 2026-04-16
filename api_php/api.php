6f21436ed43522825beb0c290b0737acefdc41d8
joaogablira <joaogab.lira24@gmail.com>
2026-04-16 09:40:10 -0300

    salvando

--- api_php/api.php
+++ api_php/api.php
@@ -6,12 +6,32 @@
 
     // echo "Método da requisição: " . $metodo;
 
+    // //CONTEÚDO
+    $usuarios = [
+        ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
+        ["id" => 2, "nome" => "João Silva", "email" => "joao@email.com"]
+    ];
+
     switch ($metodo) {
         case 'GET':
-            echo "AQUI AÇÕES DO MÉTODO GET";
+            // echo "AQUI AÇÕES DO MÉTODO GET";
+            // CONVERTE PARA JSON E RETORNA
+            echo json_encode($usuarios);
             break;
         case 'POST':
-            echo "AQUI AÇÕES DO MÉTODO POST";
+            // echo "AQUI AÇÕES DO MÉTODO POST";
+            $dados = json_decode(file_get_contents('php://input'), true);
+            // print_r($dados);
+            $novoUsuario = [
+                "id" => $dados ["id"],
+                "nome" => $dados ["nome"],
+                "email" => $dados ["email"]
+            ];
+
+            // Adiciona o novo usuário ao array existente
+            array_push($usuarios, $novoUsuario);
+            echo json_encode('Usuário inserido com sucesso!');
+            print_r($usuarios);
             break;
         default:
             echo "MÉTODO NÃO ENCONTRADO!";
@@ -19,11 +39,7 @@
     }
 
     
-    // //CONTEÚDO
-    // $usuarios = [
-    // ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
-    // ["id" => 2, "nome" => "João Silva", "email" => "joao@email.com"]
-    // ];
+    
 
     // // Converte para JSON e retorna
     // echo json_encode($usuarios);
